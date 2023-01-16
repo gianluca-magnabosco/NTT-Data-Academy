135775e61ff02362e7219f554c6516b9812c4b29
Gianluca Magnabosco <gianluca_1406@hotmail.com>
2023-01-16 10:54:44 -0300

    Exercicio 01

---
+++ Exercicio01/TestaSAPCommerce.java
@@ -0,0 +1,8 @@
+public class TestaSAPCommerce {
+    public static void main(String[] args) {
+        SAPCommerce sapCommerce = new SAPCommerce();
+
+        sapCommerce.addToCart();
+        sapCommerce.removeItemFromCart();
+    }
+}
